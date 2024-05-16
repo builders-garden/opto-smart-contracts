60f7fae6fdf831026a16987dca7249da58bc3475
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-05-16 17:12:39 +0200

    wip

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("hardhat-contract-sizer");
+
 
 const config: HardhatUserConfig = {
   solidity: {
